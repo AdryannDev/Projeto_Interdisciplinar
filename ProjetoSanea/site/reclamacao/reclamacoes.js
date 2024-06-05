24f2eec4bd83ace5cc0f406070d9f88e304a9156
Matheus <gueffmatheus@gmail.com>
2024-06-04 22:02:25 -0300

    Adicionado responsividade para Doenças

--- ProjetoSanea/site/reclamacao/reclamacoes.js
+++ ProjetoSanea/site/reclamacao/reclamacoes.js
@@ -1,56 +1,176 @@
-let btn_fil_reclamacao = document.getElementById("filtro-reclamacoes");
-let fil_reclamacao = document.getElementById("input-filtro-reclamacao");
-let cancelar_fil_reclamacao = document.getElementById("cancelar-pesquisa-reclamacao");
+let fil_reclamacao = document.getElementById("input-filtro-reclamacoes");
+let cancelar_fil_reclamacao = document.getElementById("cancelar-pesquisa-reclamacoes");
 let cards_reclamacoes = document.querySelectorAll(".card-reclamacao");
 let nenhuma_reclamacao = document.querySelector(".card-nenhuma-reclamacao");
+let resultados_reclamacao = document.querySelector(".resultado-pesquisa-reclamacoes");
+let span_resultados = resultados_reclamacao.querySelector("span");
+let select_fil_reclamacao = document.getElementById("select-reclamacoes"); 
 
 ScrollReveal().reveal('.banner-reclamacoes',{origin: "left", duration: 2000, easing: 'ease', opacity: 0.2, distance: "100px"});
 ScrollReveal().reveal('.img-banner-reclamacoes',{origin: "bottom", duration: 2000, easing: 'ease', opacity: 0.2, distance: "40px"});
 
-if(!fil_reclamacao.value){
-    cancelar_fil_reclamacao.style.display = "none";
-}
 
+//Função ao utilizar o input para escrever
 fil_reclamacao.addEventListener('input', function(){
-    let cont_resultados = cards_reclamacoes.length;
-    if(!fil_reclamacao.value){
-        cancelar_fil_reclamacao.style.display = "none";
-        nenhuma_reclamacao.style.display = "none";
+    filtrar_reclamacao(fil_reclamacao);
+    select_fil_reclamacao.value = "todos";
+});
+
+//Função ao modificar o select
+select_fil_reclamacao.addEventListener('input', function(){
+    filtrar_reclamacao(select_fil_reclamacao);
+    fil_reclamacao.value = ``;
+});
+
+function filtrar_reclamacao(pesquisa){
+    //Se o filtro estiver vazio, esconde os elementos de pesquisa
+    if(!pesquisa.value){
+        display_pesquisa("hide");
     }
-    else{
+    else{ //Senão, mostra o elemento de resetar filtro
         cancelar_fil_reclamacao.style.display = "flex";
     }
     
-    for (let i = 0; i < cards_reclamacoes.length; i++) {
-        let card_id = cards_reclamacoes[i].id;
-        if(!card_id.includes(`card-${fil_reclamacao.value.toLowerCase()}`)){
-            cards_reclamacoes[i].style.display = "none";
+    //Se for uma pesquisa utilizando o SELECT
+    if(pesquisa == select_fil_reclamacao){
+        select_filtro_reclamacao(pesquisa);
+    }
+    //Se for uma pesquisa pelo INPUT
+    else{ 
+        input_filtro_reclamacao(pesquisa);
+    }
+}
+
+function select_filtro_reclamacao(pesquisa){
+    let hasTag = false;
+    let num_resultados = 0;
+
+    if(pesquisa.value.trim().toLowerCase() == "todos"){
+        cards_reclamacoes.forEach(card => {
+            card.style.display = "flex";
+        });
+        display_pesquisa("hide");
+    } else {
+        cards_reclamacoes.forEach(card => {
+            let hasTag = false;
+            let tags = card.querySelectorAll(".tags-reclamacoes span");
+            tags.forEach(tag => {
+                tag = tag.className.trim().toLowerCase();
+                if(tag.includes(pesquisa.value.trim().toLowerCase())) {
+                    hasTag = true;
+                }
+            });
+
+            if(hasTag){
+                card.style.display = "flex";
+                num_resultados++;
+            } else {
+                card.style.display = "none";
+            }
+        });
+
+        if(num_resultados > 0){
+            cancelar_fil_reclamacao.style.display = "flex";
+            resultados_reclamacao.style.display = "flex";
+            nenhuma_reclamacao.style.display = "none";
+            if(num_resultados > 1){
+                span_resultados.innerHTML = `${num_resultados} resultados para <span class="palavra-filtro-reclamacoes"> "${pesquisa.value}"</span>`;
+            } else{
+                span_resultados.innerHTML = `${num_resultados} resultado para <span class="palavra-filtro-reclamacoes"> "${pesquisa.value}"</span>`
+            }
+        } else {
+            display_pesquisa("show");
+            span_resultados.innerHTML = `Nenhum resultado encontrado para <span class="palavra-filtro-reclamacoes"> "${pesquisa.value}"</span>`;
         }
-        else{
-            cards_reclamacoes[i].style.display = "flex";
+    }
+}
+
+
+function input_filtro_reclamacao(pesquisa){
+    cont_resultados = cards_reclamacoes.length;
+    num_resultados = 0;
+
+    cards_reclamacoes.forEach(card => {
+        let card_id = card.id;
+        if(card_id.includes(`card-${pesquisa.value.toLowerCase()}`)){ //Verif se o valor inserido é igual ao id do CARD
+            card.style.display = "flex";
             cont_resultados--;
         }
-    }
-    
-    if(cont_resultados == cards_reclamacoes.length){
-        nenhuma_reclamacao.style.display = "flex";
-    } else{
-        nenhuma_reclamacao.style.display = "none";
-    }
-});
+        else{
+            card.style.display = "none";
+            resultados_reclamacao.style.display = "flex";
+        }
+        num_resultados = cards_reclamacoes.length - cont_resultados;
 
+        if(num_resultados == 1){
+            span_resultados.innerHTML = `${num_resultados} resultado para <span class="palavra-filtro-reclamacoes"> "${pesquisa.value}"</span>`;
+            nenhuma_reclamacao.style.display = "none";
+        } else if(num_resultados > 1){
+            span_resultados.innerHTML = `${num_resultados} resultados para <span class="palavra-filtro-reclamacoes"> "${pesquisa.value}"</span>`;
+            nenhuma_reclamacao.style.display = "none";
+        } else{//Se não encontrar um resultado
+            span_resultados.innerHTML = `Nenhum resultado encontrado para <span class="palavra-filtro-reclamacoes"> "${pesquisa.value}"</span>` ;
+            nenhuma_reclamacao.style.display = "flex";
+        }
+    });
+
+    // for(let i = 0; i < cards_reclamacoes.length; i++) { //Faz para cada CARD
+    //     let card_id = cards_reclamacoes[i].id;
+    //     if(card_id.includes(`card-${pesquisa.value.toLowerCase()}`)){ //Verif se o valor inserido é igual ao id do CARD
+    //         cards_reclamacoes[i].style.display = "flex";
+    //         cont_resultados--;
+    //     }
+    //     else{
+    //         cards_reclamacoes[i].style.display = "none";
+    //         resultados_reclamacao.style.display = "flex";
+    //     }
+    //     num_resultados = cards_reclamacoes.length - cont_resultados;
+
+    //     if(num_resultados == 1){
+    //         span_resultados.innerHTML = `${num_resultados} resultados para <span class="palavra-filtro-reclamacoes"> "${pesquisa.value}"</span>`;
+    //         nenhuma_reclamacao.style.display = "none";
+    //     } else if(num_resultados > 1){
+    //         span_resultados.innerHTML = `${num_resultados} resultados para <span class="palavra-filtro-reclamacoes"> "${pesquisa.value}"</span>`;
+    //         nenhuma_reclamacao.style.display = "none";
+    //     } else{//Se não encontrar um resultado
+    //         span_resultados.innerHTML = `Nenhum resultado encontrado para <span class="palavra-filtro-reclamacoes"> "${pesquisa.value}"</span>` ;
+    //         nenhuma_reclamacao.style.display = "flex";
+    //     }
+    // }
+}
+
+//Função para resetar filtro
 cancelar_fil_reclamacao.addEventListener('click', function(){
     fil_reclamacao.value = "";
     cards_reclamacoes.forEach(card => {
         card.style.display = "flex";
     });
-    cancelar_fil_reclamacao.style.display = "none";
-    nenhuma_reclamacao.style.display = "none";
+    display_pesquisa("hide");
+    select_fil_reclamacao.value = "todos";
 });
 
+//Função para adicionar um link para cada card
 cards_reclamacoes.forEach(card => {
     card.addEventListener('click', function(){
         // window.location.href = `view_reclamacoes.html?${card.id.substring(5)}`; -> codigo para view com variavel com o nome da doença
         window.location.href = `view_reclamacoes.html`;
     });
 })
+
+//Função para alterar displays dos elementos de pesquisa
+function display_pesquisa(acao){
+    switch (acao) {
+        case "hide":
+            nenhuma_reclamacao.style.display = "none";
+            cancelar_fil_reclamacao.style.display = "none";
+            resultados_reclamacao.style.display = "none";
+            break;
+        case "show":
+            nenhuma_reclamacao.style.display = "flex";
+            cancelar_fil_reclamacao.style.display = "flex";
+            resultados_reclamacao.style.display = "flex";
+            break;
+        default:
+            break;
+    }
+}
